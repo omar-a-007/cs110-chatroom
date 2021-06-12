016e4ee797ca665e5b7720eeb50ca4f9d2d4ffcc
omar-a-007 <74106217+omar-a-007@users.noreply.github.com>
2021-06-12 08:35:37 -0400

    Update constants.js

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,11 +1,18 @@
-const SERVER_PORT = 3003
+/*
+ * Note about .env variables and React:
+ * 
+ * React does not make everything in process.env available or accessible.
+ * In order to set a process variable, make sure you append it with REACT_APP_
+ * Also, dont include quotes. Example, REACT_APP_SERVER_URL=https://chat-server-oa.herokuapp.com    
+ * 
+ */
+const { REACT_APP_SERVER_URL, REACT_APP_SERVER_PORT} = process.env
 
+const SERVER_URL         = REACT_APP_SERVER_URL  || 'http://localhost'
+const SERVER_PORT        = REACT_APP_SERVER_PORT || 3003
+const API_BASE_URL_PARAM = SERVER_URL.indexOf('localhost') !== -1 ? SERVER_URL + ":" + SERVER_PORT : SERVER_URL
 
-const os = require('os');
-let API_BASE_URL_PARAM = '';
+console.log(API_BASE_URL_PARAM)
 
-if(os.hostname().indexOf("local") > -1)
-	API_BASE_URL_PARAM = 'http://localhost:' + SERVER_PORT;
-
-export const API_BASE_URL = API_BASE_URL_PARAM;
-export const ACCESS_TOKEN_NAME = 'login_access_token';
+export const API_BASE_URL = API_BASE_URL_PARAM
+export const ACCESS_TOKEN_NAME = 'login_access_token'
