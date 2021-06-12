50bb0014bd0a08fbb973d5736f9f7d33033a8979
omar-a-007 <74106217+omar-a-007@users.noreply.github.com>
2021-06-12 09:06:22 -0400

    Update ChatRoom.js
    Better formatting. Remove undesirable fields.

--- server/models/ChatRoom.js
+++ server/models/ChatRoom.js
@@ -8,12 +8,7 @@ const ChatRoomSchema = mongoose.Schema({
 
   room_image:    { type: String },
   room_country:  { type: String,    default: 'us' }, 
-  // participants: [{type: Schema.Types.ObjectId, ref:'user'}],
-
-  // TODO remove attrbutes below?
-  male_count:    { type: Number,     default: 0   },
-  female_count:  { type: Number,     default: 0   },
-  unknown_count: { type: Number,     default: 0   },
+  // participants: [{type: Schema.Types.ObjectId, ref:'user'}], // I aggregate it in room_utils.detailed_list
 }, {
   timestamps: true, // automatically create/maintain timestamp fields:created_at, updated_at
   toObject: { virtuals: true },
