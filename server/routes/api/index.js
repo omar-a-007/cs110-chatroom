84efdd46d58d397bbeca1f64b1d05feed750f621
omar-a-007 <74106217+omar-a-007@users.noreply.github.com>
2021-06-12 09:30:07 -0400

    Update index.js

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -7,11 +7,8 @@ router.get('/', (req, res, next) => res.status(200).send('API Index'))
 // Import Routers
 var usersRouter = require('./users');
 var roomRouter = require('./room');
-var chatRoomRouter = require("./chatroom");
 
 router.use('/user', usersRouter);
 router.use('/room', roomRouter);
-router.use("/chatroom", chatRoomRouter);
-
 
 module.exports = router;
