887b25e235011eaf4754bea658b00597eb05a5b2
omar-a-007 <74106217+omar-a-007@users.noreply.github.com>
2021-06-12 09:27:38 -0400

    Update requests.js

--- client/src/utils/requests.js
+++ client/src/utils/requests.js
@@ -1,13 +1,18 @@
 const axios = require("axios")
 const APIURL = require('../constants/constants').API_BASE_URL
 
-//export const getChatRooms = () => axios.get(`${APIURL}/chatroom/chatrooms`)
-export const getChatRooms = () => axios.get(`${APIURL}/api/room/list`)
-export const getChatRoomMessages = chatRoomName =>  axios.get(`${APIURL}/chatroom/chatroom/messages/${chatRoomName}`)
+/**
+ * 
+ * @const createRoomApi     used in components: CreateRoom
+ * @const getRoomMessages   used in components: ChatRoom
+ * @const getChatRooms      used in components: HomePage
+ * @const joinChatRoom      used in components: HomePage, CreateRoom
 
-export const joinRoom = room => axios.post(`${APIURL}/api/chatroom/chatroom`, { room })
-
+ */
+export const getChatRooms = () => axios.get(`${APIURL}/api/room/list`) 
 export const getRoomMessages = (roomId)        => axios.get( `${APIURL}/api/room/messages/${roomId}`)
-export const joinChatRoom    = (roomId, token) => axios.post(`${APIURL}/api/room/join`,   { roomId }, { headers: { 'token': token}})
-export const getRoomDetail   = (roomId, token) => axios.post(`${APIURL}/api/room/detail`, { roomId }, { headers: { 'token': token}})
 export const createRoomApi   = (name, token)   => axios.post(`${APIURL}/api/room/create`, { name },   { headers: { 'token': token}})
+
+// ! Will be deprecated
+export const joinChatRoom    = (roomId, token) => axios.post(`${APIURL}/api/room/join`,   { roomId }, { headers: { 'token': token}})
+//export const getRoomDetail   = (roomId, token) => axios.post(`${APIURL}/api/room/detail`, { roomId }, { headers: { 'token': token}})
